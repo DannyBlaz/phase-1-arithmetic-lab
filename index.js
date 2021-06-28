a1e6a5e26a4120dfead0ee24a6bb3ca27d78ef18
Daniel <dannyblaze469@gmail.com>
2021-06-28 12:23:58 -0400

    commit tree e27e6f4d5c1f38787dab20579c2e2f3b82536072

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const num3 = 10
+const num4 = 6
+const numbers = [4,13, 5, 20, 15]
+const multiply = num1 * num2
+
+let rand = Math.round(Math.random() * 10 + 1)
+function random() {
+    if (rand == false) {
+        return(true)
+    } else {
+        return(rand)
+    }
+}
+
+random()
+
+const mod = num3 % num4
+
+const max = Math.max(...numbers)
